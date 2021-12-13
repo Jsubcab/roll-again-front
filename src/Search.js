0d4877591ed188d657e7569779e3aa449e4525e5
Jsubcab <jsubcab@gmail.com>
2021-12-13 23:49:47 +0100

    [TFG-1] - Gathering API data on search bar

--- src/Search.js
+++ src/Search.js
@@ -2,41 +2,93 @@ import './Search.css';
 import Data from "./mock-data.json";
 import {Container, Row, Card, Button} from 'react-bootstrap';
 import {useState} from "react";
+import React, {Component} from 'react';
+import axios from './axios';
 
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        Products: []
+    };
+}
 
-function Search() {
-    const [query, setQuery] = useState("")
+getUsersData() {
+  axios
+      .get(`/products`, {})
+      .then(res => {
+          const data = res.data
+          console.log(data)
+
+          const products = data.map(u =>
+            <div key={u.id}>
+            <p>{u.id}</p>
+            <p>{u.name}</p>
+            <p>{u.description}</p>
+            <p>{u.state}</p>
+            </div>
+            )
+            
+            this.setState({products})
+
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+
+}
+
+componentDidMount(){
+  this.getUsersData()
+}
+
+render() {
     return (
-<div className="search-panel">
-<input placeholder="Enter Post title" onChange={event => setQuery(event.target.value)} />
-<Container>
-    <Row xs={2} md={4}>
-{
-    Data.filter(post => {
-        if (query === '') {
-          return post;
-        } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
-          return post;
-        }
-      }).map((post) => (
-      
-    <Card key={post.id} style={{ width: '7rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>{post.title}</Card.Title>
-      <Card.Text>
-      {post.author}
-      </Card.Text>
-      <Button variant="primary">View</Button>
-    </Card.Body>
-  </Card>
-  ))
-}
-</Row>
-</Container>
-</div>
+      <div>
+        {this.state.products}
+      </div>
     );
-    
-}
+  }
 
-export default Search;
+}
+  // const getAllProducts = () => {
+  //   axios.get(`${productsURL}products`)
+  //   .then((response) => {
+  //     const allProducts = response.data.query.allProducts;
+  //   })
+  //   .catch(error => console.error(`Error: ${error}`));
+  // }
+
+
+//     const [query, setQuery] = useState("")
+//     return (
+// <div className="search-panel">
+// <input placeholder="Enter Post title" onChange={event => setQuery(event.target.value)} />
+// <Container>
+//     <Row xs={2} md={4}>
+// {
+//     Data.filter(post => {
+//         if (query === '') {
+//           return post;
+//         } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+//           return post;
+//         }
+//       }).map((post) => (
+      
+//     <Card key={post.id} style={{ width: '7rem' }}>
+//     <Card.Img variant="top" src="holder.js/100px180" />
+//     <Card.Body>
+//       <Card.Title>{post.title}</Card.Title>
+//       <Card.Text>
+//       {post.author}
+//       </Card.Text>
+//       <Button variant="primary">View</Button>
+//     </Card.Body>
+//   </Card>
+//   ))
+// }
+// </Row>
+// </Container>
+// </div>
+//     );
+    
